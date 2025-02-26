7c75cf775a31b45a4de62bf9a77e1dfc17091c89
Nicholas Bartelo <77738340+Nicholas-Bartelo@users.noreply.github.com>
2025-02-26 21:15:16 +0000

    Add test.py

--- test.py
+++ test.py
@@ -6,3 +6,17 @@ print("hello world")
 
 print("You are in the test.py file")
 
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [5, 7, 6, 8, 7]
+
+# Create scatter plot
+plt.scatter(x, y, color='blue', marker='o')
+
+# Add labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Scatter Plot')
+
+# Show the plot
+plt.show()
